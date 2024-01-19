91449b3bc11218691588fe70a43a8522d2b7e07b
sadiebybee <byb22004@byui.edu>
2024-01-19 15:20:31 -0700

    step 1 & 2

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,31 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullname ='Sadie Bybee';
+
+let currentyear = '2024';
+
+let profilepicture = 'images/sadie.png';
+
+/* Step 3 - Element Variables */
+
+
+
+
+
+/* Step 4 - Adding Content */
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
