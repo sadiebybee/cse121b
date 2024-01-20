b03cb6288971944409a3e02e028e9dee9b545adb
sadiebybee <byb22004@byui.edu>
2024-01-20 15:42:25 -0700

    updated

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -3,29 +3,40 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-let fullname ='Sadie Bybee';
+let fullName ='Sadie Bybee';
 
-let currentyear = '2024';
+let currentYear = '2024';
 
-let profilepicture = 'images/sadie.png';
+let profilePicture = 'images/sadie.png';
 
 /* Step 3 - Element Variables */
+const nameElement = document.getElementById('name')
 
+const foodElement = document.getElementById('food')
 
+const yearElement = document.querySelector('#year')
 
-
+document.querySelector('img').setAttribute('src', profilePicture);
 
 /* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
 
-
-
-
-
+document.querySelector('#year').textContent = currentYear
 
 /* Step 5 - Array */
+const favoriteFoods = ['Chicken', 'Soup', 'Fajitas', 'Fries', 'Brownies'];
+document.getElementById('food').textContent = favoriteFoods;
 
+const anotherFavoriteFood = 'Hamburger';
 
+favoriteFoods.push(anotherFavoriteFood);
 
+foodElement.innerHTML += `<br>${favoriteFoods}`;
 
+favoriteFoods.shift();
 
+foodElement.innerHTML += `<br>${favoriteFoods}`;
 
+favoriteFoods.pop();
+
+foodElement.innerHTML += `<br>${favoriteFoods}`;
