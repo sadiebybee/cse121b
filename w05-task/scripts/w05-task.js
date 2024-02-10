65cfa1626ac13d06c30cfa949fb395f547ebce4a
sadiebybee <byb22004@byui.edu>
2024-02-09 17:21:16 -0700

    task 5

---
+++ w05-task/scripts/w05-task.js
@@ -0,0 +1,71 @@
+/* W05: Programming Tasks */
+
+/* Declare and initialize global variables */
+const templesElement = document.querySelector('#temples');
+let templeList = {};
+
+/* async displayTemples Function */
+const displayTemples = (temples) => {
+    temples.forEach((temple) => {
+        let article = document.createElement("article");
+
+        let h3 = document.createElement("h3");
+        h3.textContent = temple.templeName;
+
+        let img = document.createElement("img");
+        img.setAttribute("src", temple.imageUrl);
+        img.setAttribute("alt", temple.templeName);
+
+        article.appendChild(h3);
+        article.appendChild(img);
+        templesElement.appendChild(article);
+    });
+};
+
+/* async getTemples Function using fetch()*/
+const getTemples = async () => {
+    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+    if (response.ok) {
+        templeList = await response.json();
+    }
+    displayTemples(templeList);
+};
+
+/* reset Function */
+const reset = () => {
+    templesElement.innerHTML = "";
+};
+
+/* filterTemples Function */
+const filterTemples = (temples) =>  {
+    reset();
+
+    let filter = document.getElementById("filtered").value;
+
+    switch (filter) {
+        case "utah":
+            displayTemples(temples.filter(temple => temple.location.toLowerCase().includes("utah")));
+            break;
+        
+        case "notutah":
+            displayTemples(temples.filter(temple => !temple.location.toLowerCase().includes("utah")));
+            break;
+
+        case "older":
+            displayTemples(temples.filter(temple => new Date(temple.dedicated) < new Date(1950, 0, 1)));
+            break;
+
+        case "all":
+            displayTemples(temples);
+            break;
+
+        default:
+            break;
+    }
+    return temples;
+};
+
+getTemples();
+
+/* Event Listener */
+document.querySelector("#filtered").addEventListener("change", () => { filterTemples(Object.values(templeList)) });
